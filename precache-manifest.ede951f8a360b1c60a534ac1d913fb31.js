e92136f5f69d390918fb479af2ed4e2fc545a4e0
Ray Phillips <raydphillips3@gmail.com>
2020-08-07 17:12:03 -0500

    Updates

---
+++ precache-manifest.ede951f8a360b1c60a534ac1d913fb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1f9c9c4d29fdfe09e87e4642d075fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1444af8c02837eee128",
+    "url": "/static/css/main.1daf052d.chunk.css"
+  },
+  {
+    "revision": "376dc4622215ca996a2b",
+    "url": "/static/js/2.bfcd0f49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bfcd0f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1444af8c02837eee128",
+    "url": "/static/js/main.ebf0b3c1.chunk.js"
+  },
+  {
+    "revision": "b20752ee13da7c74eb6a",
+    "url": "/static/js/runtime-main.1ccbeae9.js"
+  }
+]);
